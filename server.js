71af7462eccf63ed3f0e7cc704c70e752c9c323e
lucianoortizsilva <lucianoortizsilva@gmail.com>
2020-05-12 21:44:42 -0300

    Criando endpoint Hello World com Node

--- server.js
+++ server.js
@@ -8,6 +8,16 @@ app.use(express.static(__dirname + '/dist/medida-corporal'));
 
 app.get('/*', (req, res) => res.sendFile(path.join(__dirname)));
 
+/**
+ * 
+ * Endpoint Hello World
+ * 
+ */
+app.get('/helloworld', (req, res) => {
+    res.type('application/json');
+    res.send('{peso: 82.2}');
+});
+
 const server = http.createServer(app);
 
-server.listen(port, ()=> console.log('Rodando na porta: ', port));
+server.listen(port, ()=> console.log('Rodando... porta: ', port));
