df8b5cd1a6fd69d11687953c416e277bfbf2f44d
lucianoortizsilva <lucianoortizsilva@gmail.com>
2020-06-12 18:47:42 -0300

    Ajustes Ambiente

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   medidasAPI: 'http://localhost:3000/medidas',
   usuariosAPI: 'http://localhost:3000/usuarios',
-  GOOGLE_PROVIDER_ID: '359998324820-o21gal2e6nglh96u39j0u1r2qud8eqg0.apps.googleusercontent.com'
+  GOOGLE_PROVIDER_ID: ''
 };
