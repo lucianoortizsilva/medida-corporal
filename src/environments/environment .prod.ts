6037cd878b7a8a76a038d224c3bf571e4d5e2deb
lucianoortizsilva <lucianoortizsilva@gmail.com>
2020-06-12 18:46:54 -0300

    Atualizando variáveis de ambiente

---
+++ src/environments/environment .prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  medidasAPI: '',
+  usuariosAPI: '',
+  GOOGLE_PROVIDER_ID: ''
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  production: true,
-  medidasAPI: process.env.API_MEDIDAS || 'http://localhost:3000/medidas',
-  usuariosAPI: process.env.API_USUARIOS || 'http://localhost:3000/usuarios',
-  GOOGLE_PROVIDER_ID: process.env.GOOGLE_PROVIDER_ID || ''
+  production: false,
+  medidasAPI: 'http://localhost:3000/medidas',
+  usuariosAPI: 'http://localhost:3000/usuarios',
+  GOOGLE_PROVIDER_ID: '359998324820-o21gal2e6nglh96u39j0u1r2qud8eqg0.apps.googleusercontent.com'
 };
