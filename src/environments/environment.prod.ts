10c407b13912893aa99f6ecee93729179d8a298e
lucianoortizsilva <lucianoortizsilva@gmail.com>
2020-07-09 20:31:58 -0300

    update environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   medidasAPI: 'https://lucianoortizsilva-dev.com/medida-corporal/medidas',
   usuariosAPI: 'https://lucianoortizsilva-dev.com/medida-corporal/usuarios',
-  GOOGLE_PROVIDER_ID: '85887977533-3p5mfjpe2e22a4l01p6b167ggmip9ujv.apps.googleusercontent.com'
+  GOOGLE_PROVIDER_ID: '?????????? adicione sua chave'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   medidasAPI: 'http://localhost:3000/medida-corporal/medidas',
   usuariosAPI: 'http://localhost:3000/medida-corporal/usuarios',
-  GOOGLE_PROVIDER_ID: '85887977533-3p5mfjpe2e22a4l01p6b167ggmip9ujv.apps.googleusercontent.com'
+  GOOGLE_PROVIDER_ID: '?????????? adicione sua chave'
 };
